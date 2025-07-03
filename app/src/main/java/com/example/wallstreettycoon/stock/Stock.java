42e00797c54cb3a499bbcea1afc08e4bb90598b5
Gareth Munnings <117756714+garethmunnings@users.noreply.github.com>
2025-07-03 09:52:36 +0200

    Created Stock class

---
+++ app/src/main/java/com/example/wallstreettycoon/stock/Stock.java
@@ -0,0 +1,81 @@
+package com.example.wallstreettycoon.stock;
+
+import java.math.BigDecimal;
+
+public class Stock {
+    private int stockID;
+    private String stockName;
+    private String symbol;
+    private String category;
+    private String description;
+    private BigDecimal stockPrice;
+
+    //Constructor with all parameters
+    public Stock(int stockID, String stockName, String symbol, String category, String description, BigDecimal stockPrice){
+        this.stockID = stockID;
+        this.stockName = stockName;
+        this.symbol = symbol;
+        this.category = category;
+        this.description = description;
+        this.stockPrice =  stockPrice;
+    }
+
+    //Constuctor with only stockID (creates null stock if needed)
+    public Stock(int stockID){
+        this.stockID = stockID;
+    }
+
+    // Getter and Setter for stockID
+    public int getStockID() {
+        return stockID;
+    }
+
+    public void setStockID(int stockID) {
+        this.stockID = stockID;
+    }
+
+    // Getter and Setter for stockName
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    // Getter and Setter for symbol
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    // Getter and Setter for category
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    // Getter and Setter for description
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // Getter and Setter for stockPrice
+    public BigDecimal getStockPrice() {
+        return stockPrice;
+    }
+
+    public void setStockPrice(BigDecimal stockPrice) {
+        this.stockPrice = stockPrice;
+    }
+}
