fb01467f0071a4e51464ea3d95a00da569895429
Gareth Munnings <117756714+garethmunnings@users.noreply.github.com>
2025-07-03 10:14:39 +0200

    created StockPriceHistory

---
+++ app/src/main/java/com/example/wallstreettycoon/stock/StockPriceHistory.java
@@ -0,0 +1,12 @@
+package com.example.wallstreettycoon.stock;
+
+import java.math.BigDecimal;
+
+public class StockPriceHistory {
+    private BigDecimal priceID;
+    private int chapterStockID;
+    private int timeIncreaseStamp;
+    private int percentageInc;
+    private int timeDecreaseStamp;
+    private int percentageDec;
+}
